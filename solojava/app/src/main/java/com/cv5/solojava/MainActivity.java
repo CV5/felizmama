ec6617932430a38efb3c0c22711e435f5c522d27
Cristian Veras <mrchrist85@gmail.com>
2019-05-29 16:04:22 -0400

    Seek Bar and Ratign bar

--- solojava/app/src/main/java/com/cv5/solojava/MainActivity.java
+++ solojava/app/src/main/java/com/cv5/solojava/MainActivity.java
@@ -3,7 +3,10 @@ package com.cv5.solojava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +15,12 @@ public class MainActivity extends AppCompatActivity {
     EditText nombreEt;
     TextView factura;
     String nombre;
+    SeekBar seek_bar;
+    CheckBox checkBox;
     int contador = 0;
     int precioTasa = 2;
     int total = 0;
+    RatingBar ratingBar;
 
 
 
@@ -27,12 +33,20 @@ public class MainActivity extends AppCompatActivity {
         cantidad = findViewById(R.id.cantidad);
         nombreEt = findViewById(R.id.nombreET);
         factura = findViewById(R.id.terminarOrdenTextView);
+        checkBox = findViewById(R.id.checkbox);
+        ratingBar = findViewById(R.id.rating_bar);
+        seek_bar = findViewById(R.id.seek_bar);
       }
 
     public void ordenar(View view) {
         contador = contador + 1;
         cantidad.setText("" + contador);
-        total = contador * precioTasa;
+        if (checkBox.isChecked()){
+            precioTasa = 4;
+        }else{
+            precioTasa = 2;
+        }
+        total = total + precioTasa;
         precioTotal.setText("$" + total);
     }
 
@@ -41,7 +55,10 @@ public class MainActivity extends AppCompatActivity {
 
         nombre = nombreEt.getText().toString();
         factura.setText(nombre+" ordeno "+ contador + " tasa de cafe el monto a  pagar es $"+ total);
+        float vote = seek_bar.getProgress();
 
+        ratingBar.setRating(vote);
+        ratingBar.setVisibility(View.VISIBLE);
 
     }
 }
